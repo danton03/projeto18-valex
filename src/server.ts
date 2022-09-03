6196795ea7ccfc12d523b13ce0292220ecbe5161
Danton Matheus <dantoncosta03@hotmail.com>
2022-09-03 17:02:43 -0300

    :sparkles: feat: valida os schemas para criação de cartão

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
 import cors from 'cors';
-import express, {json} from 'express';
 import dotenv from "dotenv";
+import express, {json} from 'express';
+import 'express-async-errors';
+import errorHandler from './middlewares/errorHandler';
+import router from './routes/routes';
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(json());
+app.use(router);
+app.use(errorHandler);
 
 const PORT: number = Number(process.env.PORT);
 
